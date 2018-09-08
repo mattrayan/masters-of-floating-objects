374ee9108ff4df1fd6eb1bda99243c77776d1d25
Matt Rayan <matt.rayan@advisorstream.com>
2018-09-08 13:25:29 -0400

    Contact app, Message model and admin

---
+++ contact/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Message(models.Model):
+
+    name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.CharField(max_length=50, blank=False, null=False)
+    message = models.TextField(default='', blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
--- mofos/settings.py
+++ mofos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'rest_framework',
 
+    'contact',
     'content',
 ]
 
