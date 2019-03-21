e0a77cf5292360d64d7a881a2385803a3e4e1e1b
Matt Rayan <matt.rayan@advisorstream.com>
2019-03-21 14:52:24 -0400

    Update css/script tag rendering

--- mofos/views.py
+++ mofos/views.py
@@ -1,5 +1,26 @@
+from django.conf import settings
 from django.shortcuts import render
+
 
 def spa(request, pk=None):
 
-    return render(request, 'index.html')
+    if settings.DEBUG:
+        render_css = ''
+        render_scripts = \
+            '<script type="text/javascript" src="/static/inline.bundle.js"></script> \
+            <script type="text/javascript" src="/static/polyfills.bundle.js"></script> \
+            <script type="text/javascript" src="/static/styles.bundle.js"></script> \
+            <script type="text/javascript" src="/static/scripts.bundle.js"></script> \
+            <script type="text/javascript" src="/static/vendor.bundle.js"></script> \
+            <script type="text/javascript" src="/static/main.bundle.js"></script>'
+    else:
+        render_css = '<link rel="stylesheet" href="/static/styles.bundle.css">'
+        render_scripts = \
+            '<script type="text/javascript" src="/static/inline.bundle.js"></script> \
+            <script type="text/javascript" src="/static/polyfills.bundle.js"></script> \
+            <script type="text/javascript" src="/static/scripts.bundle.js"></script> \
+            <script type="text/javascript" src="/static/vendor.bundle.js"></script> \
+            <script type="text/javascript" src="/static/main.bundle.js"></script>'
+
+    return render(request, 'index.html',
+        context={'render_css': render_css, 'render_scripts': render_scripts})
